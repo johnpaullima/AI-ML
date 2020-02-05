fb07afe891f727100995b363026521530d96ca2a
johnpaullima <45721283+johnpaullima@users.noreply.github.com>
2020-02-05 15:57:22 -0300

    Create linear_reg.py

---
+++ linear_reg.py
@@ -0,0 +1,30 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+x = np.array([5, 15, 25, 35, 45, 55]).reshape((-1, 1))
+y = np.array([5, 20, 14, 32, 22, 38])
+
+print(x)
+
+model = LinearRegression()
+
+model.fit(x, y)
+
+model = LinearRegression().fit(x, y)
+
+r_sq = model.score(x, y)
+print('coefficient of determination:', r_sq)
+
+print('intercept:', model.intercept_)
+
+print('slope:', model.coef_)
+
+new_model = LinearRegression().fit(x, y.reshape((-1, 1)))
+
+print('intercept:', new_model.intercept_)
+
+print('slope:', new_model.coef_)
+
+y_pred = model.predict(x)
+
+print('predicted response:', y_pred, sep='\n')
